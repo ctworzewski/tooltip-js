00b0b1406e9eb744ef97ea6a0d8ab4822aba03f3
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-08-03 06:54:51 +0200

    Add atribute data to div and create element for hover square

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ const boxes = document.querySelectorAll('.boxes div');
 
 
 const createTooltip = (e) => {
+
     const tooltipParrent = e.target;
-    console.log(tooltipParrent);
+    const tooltipText = e.target.dataset.tooltip;
+    // console.log(tooltipParrent);
+    console.log(tooltipText);
+
+    const newTooltip = document.createElement('span');
+    newTooltip.innerHTML = tooltipText;
+    newTooltip.classList = 'tooltip';
+    tooltipParrent.appendChild(newTooltip);
 }
 boxes.forEach(box => box.addEventListener('mouseover', createTooltip));
