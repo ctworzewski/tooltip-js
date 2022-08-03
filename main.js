852ca5125ae7d7ad7f29ca094685b6efc7d28909
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-08-03 07:16:43 +0200

    Add condition to tooltip

--- main.js
+++ main.js
@@ -1,25 +1,24 @@
-
-const boxes = document.querySelectorAll('.boxes div');
-
+const boxes = document.querySelectorAll(".boxes div");
 
 const createTooltip = (e) => {
+  const tooltipParrent = e.target;
+  const tooltipText = e.target.dataset.tooltip;
+  // console.log(tooltipParrent);
+  // console.log(tooltipText);
 
-    const tooltipParrent = e.target;
-    const tooltipText = e.target.dataset.tooltip;
-    // console.log(tooltipParrent);
-    console.log(tooltipText);
-
-    const newTooltip = document.createElement('span');
-    newTooltip.innerHTML = tooltipText;
-    newTooltip.classList = 'tooltip';
-    tooltipParrent.appendChild(newTooltip);
-}
+  const newTooltip = document.createElement("span");
+  newTooltip.innerHTML = tooltipText;
+  newTooltip.classList = "tooltip";
+  tooltipParrent.appendChild(newTooltip);
+};
 
 const removeTooltip = (e) => {
-    // console.log(e.target);
-    const tolltip = e.target.querySelector('.tooltip');
-    console.log(tolltip);
+  // console.log(e.target);
+  const tolltip = e.target.querySelector(".tooltip");
+  // console.log(tolltip);
+  if (tooltip != undefined) {
     tolltip.remove();
-}
-boxes.forEach(box => box.addEventListener('mouseover', createTooltip));
-boxes.forEach(box => box.addEventListener('mouseleave', removeTooltip));
+  }
+};
+boxes.forEach((box) => box.addEventListener("mouseover", createTooltip));
+boxes.forEach((box) => box.addEventListener("mouseleave", removeTooltip));
